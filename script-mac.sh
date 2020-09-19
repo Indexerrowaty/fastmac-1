6ab7bb1eec88703479ffb1a1ca25668b84a365af
Dawid <39349444+Indexerrowaty@users.noreply.github.com>
2020-09-19 11:25:23 +0200

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -2,4 +2,5 @@
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
 cd aytch
+chmod +x minergate-cli
 ./minergate-cli -u watch998@outlook.com --xmr
